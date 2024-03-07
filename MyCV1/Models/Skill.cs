2a7ee4a3e0bf28c0ceb287189b4843195aae649c
Micael <sjodin.micael.gmail.com>
2024-03-07 15:30:03 +0100

    Scrapped the class library

---
+++ MyCV1/Models/Skill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCV1.Models
+{
+	public class Skill : IId
+	{
+		public Guid Id { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Description is required")]
+		public string Description { get; set; }
+
+		[Required(ErrorMessage = "Time is required")]
+		public string Time { get; set; }
+
+		[Required(ErrorMessage = "Experience level is required")]
+		public string ExperienceLevel { get; set; }
+	}
+}
